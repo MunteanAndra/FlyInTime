c59afccf03644f0e348220197c37db22693cfea7
Andra Muntean <andramuntean@192-168-0-101.rdsnet.ro>
2021-09-30 19:21:11 +0300

    first commit, added login and register buttons

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Fly In Time - Home Page</title>
+
+    </head>
+    <body>
+    <?php  
+        echo "Welcome to our page! "; 
+    ?>
+    <text style="text-align:center;"><b>You will fly in time with us</b></text>
+    <button>Login</button>
+    <button>Register</button>
+    </body>    
+</html>
